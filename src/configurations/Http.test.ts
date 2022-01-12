e5e635e386d8dc54533ba73c988e7f49f582069c
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-12 15:43:09 +0300

    implement tests for http config

--- src/configurations/Http.test.ts
+++ src/configurations/Http.test.ts
@@ -1,5 +1,37 @@
 import { HttpConfig } from './Http';
 
-test('Test default Domain name', () => {
+test('The default value for the DOMAIN name is set to.', () => {
   expect(HttpConfig.DOMAIN).toBe('localhost');
+});
+
+test('The default value for the PORT is set to.', () => {
+  expect(HttpConfig.PORT).toBe(3000);
+});
+
+test('The default value for the COMPRESSION_LEVEL is set to.', () => {
+  expect(HttpConfig.COMPRESSION_LEVEL).toBe(0);
+});
+
+test('The default value for the MAX_REQUEST_BODY_SIZE is set to.', () => {
+  expect(HttpConfig.MAX_REQUEST_BODY_SIZE).toBe('2mb');
+});
+
+test('The default value for the MAX_REQUEST_URL_SIZE is set to.', () => {
+  expect(HttpConfig.MAX_REQUEST_URL_SIZE).toBe('1kb');
+});
+
+test('The default value for the IS_URL_EXTENDED is set to.', () => {
+  expect(HttpConfig.IS_URL_EXTENDED).toBe(true);
+});
+
+test('The default value for the RATE_LIMIT_WINDOW_MS is set to.', () => {
+  expect(HttpConfig.RATE_LIMIT_WINDOWS_MS).toBe(6000);
+});
+
+test('The default value for the RATE_LIMIT_MAX_REQUESTS is set to.', () => {
+  expect(HttpConfig.RATE_LIMIT_MAX_REQUESTS).toBe(100);
+});
+
+test('The default value for the BASIC_AUTH_PASSWORD is set to.', () => {
+  expect(HttpConfig.BASIC_AUTH_PASSWORD).toBe('password');
 });
