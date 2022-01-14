90a6513b6105e787d1d747e181656726009984b7
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-14 13:28:32 +0300

    Implement debug mode for access logger

--- src/middlewares/AccessLogger.ts
+++ src/middlewares/AccessLogger.ts
@@ -1,10 +1,16 @@
-import { AccessLogger } from '../providers/Logger';
+import { AccessLogger } from '../loggers/AccessLogger';
 import morgan from 'morgan';
 import { Middleware } from '../interfaces/express';
+import { AppConfig } from '../configurations/App';
+import { DebugLogger } from '../loggers/DebugLogger';
 
 class MorganStream {
   write(text: string) {
     AccessLogger.info(text);
+
+    if (!AppConfig.IS_PRODUCTION) {
+      DebugLogger.debug(text);
+    }
   }
 }
 
