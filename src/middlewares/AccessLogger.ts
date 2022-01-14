8584bcc29dc279774c91f79c4f79c03f2dc3ab3e
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-14 15:52:32 +0300

    Done access logger

--- src/middlewares/AccessLogger.ts
+++ src/middlewares/AccessLogger.ts
@@ -1,16 +1,12 @@
 import { AccessLogger } from '../loggers/AccessLogger';
 import morgan from 'morgan';
 import { Middleware } from '../interfaces/express';
-import { AppConfig } from '../configurations/App';
 import { DebugLogger } from '../loggers/DebugLogger';
 
 class MorganStream {
   write(text: string) {
     AccessLogger.info(text);
-
-    if (!AppConfig.IS_PRODUCTION) {
-      DebugLogger.debug(text);
-    }
+    DebugLogger.debug(text);
   }
 }
 
