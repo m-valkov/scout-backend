22fa60869cadc711c0af14aff93b5c2185fdef85
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-14 13:28:11 +0300

    Add error logger constants

--- src/configurations/Logger.ts
+++ src/configurations/Logger.ts
@@ -2,4 +2,8 @@ export class LoggerConfig {
   static readonly ACCESS_LOG_MAX_FILE_SIZE_IN_BYTES: number = Number(process.env.ACCESS_LOG_MAX_FILE_SIZE_IN_BYTES || 10_485_760);
 
   static readonly ACCESS_LOG_MAX_FILES: number = Number(process.env.ACCESS_LOG_MAX_FILES) || 5;
+
+  static readonly ERROR_LOG_MAX_FILE_SIZE_IN_BYTES: number = Number(process.env.ERROR_LOG_MAX_FILE_SIZE_IN_BYTES || 10_485_760);
+
+  static readonly ERROR_LOG_MAX_FILES: number = Number(process.env.ERROR_LOG_MAX_FILES) || 5;
 }
