92ef53207f4495edf3d0645666e03d1c932a66f5
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-14 15:51:54 +0300

    Enable silent in production

--- src/loggers/DebugLogger.ts
+++ src/loggers/DebugLogger.ts
@@ -1,4 +1,5 @@
 import { createLogger, transports, LoggerOptions, Logger, format } from 'winston';
+import { AppConfig } from '../configurations/App';
 
 const { combine, printf } = format;
 
@@ -8,6 +9,7 @@ const accessLoggerOptions: LoggerOptions = {
     new transports.Console({
       level: 'debug',
       handleExceptions: true,
+      silent: AppConfig.IS_PRODUCTION,
     }),
   ],
 };
