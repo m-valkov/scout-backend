e9c38b2d979acb20a929442179b88a7ec188e563
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-16 01:20:54 +0300

    add colours to debug output

--- src/loggers/DebugLogger.ts
+++ src/loggers/DebugLogger.ts
@@ -1,12 +1,13 @@
 import { createLogger, transports, LoggerOptions, Logger, format } from 'winston';
 import { AppConfig } from '../configurations/App';
 
-const { combine, printf, timestamp } = format;
+const { combine, printf, timestamp, colorize } = format;
 
 const accessLoggerOptions: LoggerOptions = {
   format: combine(
+    colorize({ all: true }),
     timestamp({ format: 'DD.MM.YYYY HH:mm:ss' }),
-    printf(info => `${info.level.toUpperCase()} :: ${info.timestamp} :: ${info.message}`),
+    printf(info => `${info.level} :: ${info.timestamp} :: ${info.message}`),
   ),
   transports: [
     new transports.Console({
