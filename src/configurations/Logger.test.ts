a9393288441ec95815fd877028f4a3e8d7dcaa29
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-15 22:32:57 +0300

    Remove silly tests

--- src/configurations/Logger.test.ts
+++
@@ -1,9 +0,0 @@
-import { LoggerConfig } from './Logger';
-
-test('The default value for the ACCESS_LOG_MAX_FILE_SIZE_IN_BYTES name is set to.', () => {
-  expect(LoggerConfig.ACCESS_LOG_MAX_FILE_SIZE_IN_BYTES).toBe(10_485_760);
-});
-
-test('The default value for the ACCESS_LOG_MAX_FILES name is set to.', () => {
-  expect(LoggerConfig.ACCESS_LOG_MAX_FILES).toBe(5);
-});
