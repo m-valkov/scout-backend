45e7ddd5d5a9eba4fc8bbff1ce12b9cc18a5a15d
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-16 16:06:56 +0300

    add environment for sentry logger

--- src/loggers/SentryLogger.ts
+++ src/loggers/SentryLogger.ts
@@ -1,10 +1,12 @@
 import { createLogger, LoggerOptions, Logger } from 'winston';
 import Sentry from 'winston-sentry-log';
+import { AppConfig } from '../configurations/App';
 import { LoggerConfig } from '../configurations/Logger';
 
 const sentryOptions = {
   config: {
     dsn: LoggerConfig.SENTRY_DSN,
+    environment: AppConfig.IS_PRODUCTION ? 'production' : 'developing',
   },
   level: 'error',
 };
