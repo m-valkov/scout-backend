c57a02065b3dafc4ef004c207abcff0ac2d218c4
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-18 01:28:44 +0300

    Change error message

--- src/handlers/Api404Handler.ts
+++ src/handlers/Api404Handler.ts
@@ -3,6 +3,6 @@ import { Api404Error } from '../exceptions/Api404';
 import { Middleware } from '../interfaces/express';
 
 export const Api404Handler: Middleware = (req: Request, _res: Response, next: NextFunction): void => {
-  const err = new Api404Error(`Route [ ${req.url} ] not found`);
+  const err = new Api404Error(`${req.url} :: route not found`);
   next(err);
 };
