da3fcc2ea0831f0671e9422d0b5ab85468025b26
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-18 01:10:45 +0300

    add computed default property for swagger server value

--- src/configurations/Swagger.ts
+++ src/configurations/Swagger.ts
@@ -1,11 +1,15 @@
+import { APIConfig } from './Api';
+import { HttpConfig } from './Http';
+
 export class SwaggerConfig {
+  private static _defaultServerUrl = `http://${HttpConfig.DOMAIN}:${HttpConfig.PORT}${APIConfig.API_PREFIX}`;
   static readonly OPENAPI_VERSION: string = process.env.OPENAPI_VERSION || '3.0.0';
 
   static readonly SWAGGER_TITLE: string = process.env.SWAGGER_TITLE || 'Api documentation';
 
   static readonly API_VERSION: string = process.env.API_VERSION || '1.0.0';
 
-  static readonly SERVER_URL: string = process.env.SERVER_URL || 'http://localhost:3000/docs';
+  static readonly SERVER_URL: string = process.env.SERVER_URL || SwaggerConfig._defaultServerUrl;
 
   static readonly SERVER_DESCRIPTION: string = process.env.SERVER_DESCRIPTION || 'Dev server';
 
