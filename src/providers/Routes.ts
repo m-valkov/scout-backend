cadde20956413bed97aeff078281c5b03e7a9750
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-18 00:45:29 +0300

    fixes: apidoc basic auth

--- src/providers/Routes.ts
+++ src/providers/Routes.ts
@@ -2,9 +2,8 @@ import { Router } from 'express';
 import { Api } from '../routes/Api';
 import { ApiDocHandler } from '../handlers/ApiDocHandler';
 import { APIConfig } from '../configurations/Api';
-import { BasicAuth } from '../middlewares/vendor/BasicAuth';
 
 export const Routes: Router = Router();
 
 Routes.use(APIConfig.API_PREFIX, Api);
-Routes.use(APIConfig.API_PREFIX, BasicAuth, ApiDocHandler);
+Routes.use(APIConfig.API_PREFIX, ApiDocHandler);
