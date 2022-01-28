36c89f7e1fc3125b73251911fe18bcfecab24c64
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-01-28 20:15:04 +0300

    try to fixes Problem with process.exit Fixes #106

--- src/handlers/errors/CriticalErrorHandler.ts
+++ src/handlers/errors/CriticalErrorHandler.ts
@@ -5,9 +5,16 @@ import { ErrorLogger } from '../../loggers/vendor/ErrorLogger';
 import { TelegramLogger } from '../../loggers/vendor/TelegramLogger';
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const CriticalErrorHandler: ErrorRequestHandler = (err: Error | BaseError, req: Request, _res: Response, _next: NextFunction): void => {
-  TelegramLogger.log(err, req);
+export const CriticalErrorHandler: ErrorRequestHandler = (err: BaseError, req: Request, _res: Response, _next: NextFunction): void => {
+  // Write all errors in the file
   ErrorLogger.log(err, req);
+
+  // Send all non-operational errors to Telegram
+  if (!err.isOperational) {
+    TelegramLogger.log(err, req);
+  }
+
+  // Quit application if no error is detected
   if (!(err instanceof BaseError)) {
     DebugLogger.debug('Critical error. Emergency application shutdown');
     process.exit(1);
