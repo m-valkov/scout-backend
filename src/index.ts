5ad7f10d099f0a48c034ab2dd3186cf44ad01688
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-02-10 22:13:30 +0300

    refactor: move to vendor folder

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import * as ProcessControl from './helpers/ProcessControl';
+import * as ProcessControl from './helpers/vendor/ProcessControl';
 import { App } from './providers/App';
 
 new App();
