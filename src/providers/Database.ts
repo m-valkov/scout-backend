ce9d27b1b163627d24a4194787373614da9682a2
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-02-11 13:31:40 +0300

    Implement a connection to the database Fixes #139

--- src/providers/Database.ts
+++ src/providers/Database.ts
@@ -1,10 +1,20 @@
-import { connect } from 'mongoose';
+import Mongoose from 'mongoose';
 import { DebugLogger } from '../loggers/vendor/DebugLogger';
 import { Config } from './Config';
 
-const uri = new Config().DbConfig.MONGO_DB_URI;
-export const init = () => {
-  connect(uri).then(() => {
+let database: Mongoose.Connection;
+
+export const init = (): void => {
+  const uri = new Config().DbConfig.MONGO_DB_URI;
+
+  if (database) {
+    return;
+  }
+
+  Mongoose.connect(uri, {});
+  database = Mongoose.connection;
+
+  database.once('open', async () => {
     DebugLogger.debug('DB connected');
   });
 };
