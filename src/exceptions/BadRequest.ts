fb647b8c46047cbaa066a61db1eb6082f044392d
Mikhail Valkov <mikhail.valkov@gmail.com>
2022-02-20 01:12:32 +0300

    feat(Error): add BadRequest error class

---
+++ src/exceptions/BadRequest.ts
@@ -0,0 +1,8 @@
+import { BaseError } from './BaseError';
+import { HttpStatusCode } from '../configurations/HttpStatusCode';
+
+export class BadRequestError extends BaseError {
+  constructor(message: string, name = 'EmptyPayloadError', statusCode = HttpStatusCode.BAD_REQUEST, description = 'Bad request', isOperational = true) {
+    super(name, message, statusCode, isOperational, description);
+  }
+}
